0a76ac7a976d3f456e9bf8c3ae000919352ad708
legenduzair <uzairoomer@live.com>
2022-06-08 19:28:55 +0000

    Add delete product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -23,7 +23,7 @@ def profile(request):
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
-    template = 'profiles/profile.html'
+    template = 'profile.html'
     context = {
         'form': form,
         'orders': orders,
