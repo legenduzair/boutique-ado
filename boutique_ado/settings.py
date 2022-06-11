e0de9f6500642b986e043083772bbccc8ffed21d
legenduzair <uzairoomer@live.com>
2022-06-11 15:26:31 +0000

    Debug false

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['boutique-ado-uoomer.herokuapp.com', 'localhost']
 
@@ -45,8 +46,8 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    'crispy_forms',
     'profiles',
+    'crispy_forms',
     'storages',
 ]
 
